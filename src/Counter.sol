e748e31e2b1cf2e4a2da86a7dccd33a0c885071d
Aurelou <0xaurelou@gmail.com>
2023-02-27 15:10:31 +0100

    ADD errors huff

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
 
+    error TooBigNumber();
+
     function setNumber(uint256 newNumber) public {
+        require(newNumber < 10, "Counter: new number is too big");
         number = newNumber;
     }
 
-    function increment() public {
-        number++;
+    function setNumberBis(uint256 newNumber) public {
+        if(newNumber >= 10){
+            revert TooBigNumber();
+        }
+        number = newNumber;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,59 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+
+    error TooBigNumber();
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
     function testSetNumber(uint256 x) public {
+        if(x >= 10) {
+            vm.expectRevert("Counter: new number is too big");
+            counter.setNumber(x);
+            return;
+        }
         counter.setNumber(x);
         assertEq(counter.number(), x);
     }
+
+    function testSetNumberBis(uint256 x) public {
+        if(x >= 10) {
+            vm.expectRevert(abi.encodeWithSelector(TooBigNumber.selector));
+            counter.setNumberBis(x);
+            return;
+        }
+        else {
+            counter.setNumberBis(x);
+            assertEq(counter.number(), x);
+        }
+    }
+
+    function testSetNumberSpecific() public {
+        counter.setNumber(9);
+        assertEq(counter.number(), 9);
+    }
+
+    function testSetNumberSpecificFail() public {
+        vm.expectRevert("Counter: new number is too big");
+        counter.setNumber(42);
+    }
+
+    function testSetNumberBisSpecific() public {
+        counter.setNumberBis(9);
+        assertEq(counter.number(), 9);
+    }
+
+    function testSetNumberBisSpecificFail() public {
+        vm.expectRevert(abi.encodeWithSelector(TooBigNumber.selector));
+        counter.setNumberBis(42);
+    }
+
 }
